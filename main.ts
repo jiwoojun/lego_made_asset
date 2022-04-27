f594c5862db86f34ee9f5b96c426559a85ead427
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-04-27 20:41:16 +1200

    two assets added

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+scene.setBackgroundColor(0)
+let mySprite = sprites.create(assets.image`flamingo`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`fish_orange`, SpriteKind.Player)
+mySprite.setPosition(80, 55)
+mySprite2.setPosition(92, 71)
